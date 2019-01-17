75b86b825284a887afb286f7481e393121235136
LeonWu <190187791@qq.com>
2019-01-17 19:52:59 +0800

    第一次上传到本地仓库

--- src/MyGit.java
+++ src/MyGit.java
@@ -1,9 +1,9 @@
 public class MyGit {
     public static void main(String[] args) {
         System.out.println("mygit");
+        System.out.println("sdfg");
         System.out.println("mygit");
-        System.out.println("mygit");
-        System.out.println("mygit");
+        System.out.println("sdfg");
         System.out.println("mygit");
 
     }
